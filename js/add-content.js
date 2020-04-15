e165f08c0b4bfa118f2c71b37748705bda1c636b
Nedal_Erekat <nedalerekat@gmail.com>
2020-04-15 16:16:11 +0300

    whileloop

--- js/add-content.js
+++ js/add-content.js
@@ -14,21 +14,26 @@ document.write('<h1>'+'Hello'+ Name+'</h1>');
 document.write('<h4>'+today+'</h4>');*/
 
 var MovieType= function(){
+    while (choosen !== 'Action' && choosen !== 'trilling' && choosen !== 'romantic'){
     var choosen = prompt('What kind of movies would like to watch? Action,thrilling or romantic');
+    }
+    var numMovs = prompt("How many?")
     var Best;
+    for (var i=0;i<numMovs;i++){
+        console.log(numMovs);
     if (choosen === 'Action'){
-        Best = '<img src="http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_ecc7a7c9d40d2711ac254d12316ce13ce63c46fe5ab2e32fb261efe55dca9aec.jpg">';
+        Best += '<img src="http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_ecc7a7c9d40d2711ac254d12316ce13ce63c46fe5ab2e32fb261efe55dca9aec.jpg">';
     }
     else if (choosen === 'thrilling'){
-        Best = '<img src="https://i.pinimg.com/564x/39/92/63/39926312956b93e1c9dedbea15be3679.jpg">';
+        Best +=  '<img src="https://i.pinimg.com/564x/39/92/63/39926312956b93e1c9dedbea15be3679.jpg">';
     }
     else if (choosen === 'romantic'){
-        Best = '<img src="https://static.parade.com/wp-content/uploads/2020/01/Notebook.jpg">';
+        Best += '<img src="https://static.parade.com/wp-content/uploads/2020/01/Notebook.jpg">';
     }
     else {
-        Best = '<h1>Does not match, look below for other options<h1>';
+        Best += '<h1>Does not match, look below for other options<h1>';
         
     }
+    }
     return Best;
-
 }
